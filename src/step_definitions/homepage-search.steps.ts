52c86248027847e7b365ab0713da218946e00059
David Poetzsch-Heffter <davidpoetzsch@web.de>
2020-03-10 12:39:44 +0100

    converted to basic cucumber

--- src/step_definitions/homepage-search.steps.ts
+++ src/step_definitions/homepage-search.steps.ts
@@ -1,29 +1,22 @@
 import { assert } from "chai";
-import { binding, given, when, then } from "cucumber-tsflow";
 import homepage from "../pages/Homepage";
+import { Given, When, Then } from "cucumber";
 
-@binding()
-export class HomepageSearchSteps {
-  @given(/^I am on the search page$/)
-  public async givenOnHomepage() {
-    homepage.open();
-    const title = browser.getTitle();
+Given(/^I am on the search page$/, async () => {
+  homepage.open();
+  const title = browser.getTitle();
 
-    assert.equal(title, "Google");
-  }
+  assert.equal(title, "Google");
+});
 
-  @when(/^I enter "([^"]*)" into the search box$/)
-  public whenIEnterSearchText(arg1) {
-    homepage.enterText(arg1);
-  }
+When(/^I enter "([^"]*)" into the search box$/, arg1 => {
+  homepage.enterText(arg1);
+});
 
-  @when(/^I click the search button$/)
-  public whenSearchClicked() {
-    homepage.search();
-  }
+When(/^I click the search button$/, () => {
+  homepage.search();
+});
 
-  @then(/^I should see a list of search results$/)
-  public resultsShouldShow() {
-    assert.isTrue(homepage.isSearched());
-  }
-}
+Then(/^I should see a list of search results$/, () => {
+  assert.isTrue(homepage.isSearched());
+});
