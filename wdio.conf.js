4337c84446457aaf64596f9f0b37d375d782f571
David Poetzsch-Heffter <davidpoetzsch@web.de>
2020-03-12 21:17:11 +0100

    added visual regression options

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,4 +1,6 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const { addArgument } = require("@wdio/allure-reporter").default;
+const { VisualRegression } = require("wdio-visual-regression");
 const debug = !!process.env.DEBUG;
 const execArgv = debug ? ["--inspect"] : [];
 const stepTimout = debug ? 24 * 60 * 60 * 1000 : 6000;
@@ -139,7 +141,15 @@ exports.config = {
   // Services take over a specific job you don't want to take care of. They enhance
   // your test setup with almost no effort. Unlike plugins, they don't add new
   // commands. Instead, they hook themselves up into the test process.
-  services: ["selenium-standalone"],
+  services: [
+    "selenium-standalone",
+    [
+      VisualRegression,
+      {
+        /* options */
+      },
+    ],
+  ],
 
   // Framework you want to run your specs with.
   // The following are supported: Mocha, Jasmine, and Cucumber
